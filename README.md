9c89b63a4d5821c15e0a4329c1e1b4f735d77e44
dokysp <uhug@naver.com>
2019-10-23 00:15:30 +0900

    개발 완료 및 도큐멘테이션

---
+++ README.md
@@ -0,0 +1,29 @@
+# Android-Client-Test
+
+안드로이드와 백엔드 API 서버 비동기 통신 예제입니다.
+
+
+
+## 설명 
+
+- `POST`: `http://noti-drawer.run.goorm.io/api/analyze-sentence`
+
+- `object`*(body)* : 
+
+  ```javascript
+  {"sentence": "입력할 문장"}
+  ```
+
+- 서버 repo: [Link]( https://github.com/noti-dropper/backend )
+
+- 서버와의 **통신이 실패**했을 경우, **빈 문자열`""`을 반환**함 :arrow_right: 에러 처리 시 활용
+
+
+
+## 참고
+
+- [HttpURLConnection+비동기 처리 예제]( http://webs.co.kr/index.php?mid=http&document_srl=3314894 )
+
+- [AsyncTask 설명]( https://youngest-programming.tistory.com/11 )
+
+- [안드로이드 Network StrictMode 이슈 해결]( https://stackoverflow.com/questions/22395417/error-strictmodeandroidblockguardpolicy-onnetwork )
--- app/src/main/java/com/example/httpconnection/MainActivity.java
+++ app/src/main/java/com/example/httpconnection/MainActivity.java
@@ -1,74 +1,79 @@
 package com.example.httpconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-import cz.msebera.android.httpclient.NameValuePair;
-import cz.msebera.android.httpclient.client.ClientProtocolException;
-import cz.msebera.android.httpclient.client.HttpClient;
-import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
-import cz.msebera.android.httpclient.client.methods.HttpPost;
-import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
-import cz.msebera.android.httpclient.message.BasicNameValuePair;
-
-import cz.msebera.android.httpclient.params.HttpConnectionParams;
-import cz.msebera.android.httpclient.params.HttpParams;
-import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button send;
+    private TextView status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        new SendPost().execute();
 
+        super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        setContentView(R.layout.activity_main);
+
+        send = findViewById(R.id.send);
+        status = findViewById(R.id.status);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "http://noti-drawer.run.goorm.io/api/analyze-sentence";
+                String sentence = "{\"sentence\": \"오늘은 치킨 섭취를 먹구 싶어용.\"}";
+
+                GetNounInSentenceAsync getNoun = new GetNounInSentenceAsync(url, sentence);
+                getNoun.execute();
+
+                send.setEnabled(false);
+                status.setText("데이터 수신중입니다.");
+            }
+        });
     }
 
-    private class SendPost extends AsyncTask<Void, Void, String> {
-        protected String doInBackground(Void... unused) {
-            String content = executeClient();
-            return content;
+
+    public class GetNounInSentenceAsync extends AsyncTask<Void, Void, String> {
+
+        String url;
+        String sentence;
+
+        // Constructor
+        public GetNounInSentenceAsync(String url, String sentence){
+            this.sentence = sentence;
+            this.url = url;
         }
 
-        protected void onPostExecute(String result) {
-            // 모두 작업을 마치고 실행할 일 (메소드 등등)
+        @Override
+        protected String doInBackground(Void... voids) {
+            // 비동기 처리 후 결과값을 리턴
+            // 이 메소드가 끝난 후에 onPostExecute()가 실행됨
+            return new GetNownInSentence().request(url, sentence);
         }
 
-        // 실제 전송하는 부분
-        public String executeClient() {
-            ArrayList<NameValuePair> post = new ArrayList<NameValuePair>();
-            post.add(new BasicNameValuePair("id", "가나다"));
-            post.add(new BasicNameValuePair("pw", "1234"));
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            // 서버 통신 후 처리 완료
+            // 받아온 값은 result에 저장되며 JSON 타입으로 저장됨
+            // 올바르지 못한 값을 받아왔을 때, 빈 String 값이 나옴
 
-            // 연결 HttpClient 객체 생성
-            HttpClient client = new DefaultHttpClient();
-
-            // 객체 연결 설정 부분, 연결 최대시간 등등
-            HttpParams params = client.getParams();
-            HttpConnectionParams.setConnectionTimeout(params, 5000);
-            HttpConnectionParams.setSoTimeout(params, 5000);
-
-            // Post객체 생성
-            HttpPost httpPost = new HttpPost("https://noti-drawer.run.goorm.io/");
-
-            try {
-                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(post, "UTF-8");
-                httpPost.setEntity(entity);
-                client.execute(httpPost);
-                return EntityUtils.getContentCharSet(entity);
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+            send.setEnabled(true);
+            if(result.equals(""))
+                status.setText("실패하였습니다.");
+            else
+                status.setText("완료하였습니다.\n" + result);
         }
     }
 }
