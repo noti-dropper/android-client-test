4716196749691b0ad11c97943930821b610c166f
YSJ <ychysj95@gmail.com>
2019-10-23 21:09:32 +0900

    Backend wordcloud 이미지 수신 API 대응 완료

--- app/src/main/java/com/example/httpconnection/GetNownInSentence.java
+++ app/src/main/java/com/example/httpconnection/GetNownInSentence.java
@@ -1,5 +1,9 @@
 package com.example.httpconnection;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,7 +18,6 @@ import java.net.URL;
 public class GetNownInSentence {
 
     public String request(String _url, String _sentence){
-
         HttpURLConnection urlConn = null;
 
         try {
@@ -61,4 +64,52 @@ public class GetNownInSentence {
 
         return "";
     }
+
+
+    // ImgProcess
+    public Bitmap requestImg(String _url, String _sentence){
+        HttpURLConnection urlConn = null;
+        Bitmap bit = null;
+
+        try {
+
+            URL url = new URL(_url);
+
+            urlConn =  (HttpURLConnection)url.openConnection();
+
+            urlConn.setRequestMethod("POST");
+            urlConn.setRequestProperty("Content-Type", "application/json");
+
+            String strParams = _sentence;
+            OutputStream os = urlConn.getOutputStream();
+
+            os.write(strParams.getBytes("UTF-8"));
+            os.flush();
+            os.close();
+
+            if(urlConn.getResponseCode() != HttpURLConnection.HTTP_OK)
+                return null;
+
+            bit = BitmapFactory.decodeStream(urlConn.getInputStream());
+
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(urlConn != null)
+                urlConn.disconnect();
+        }
+
+        return bit;
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/httpconnection/MainActivity.java
+++ app/src/main/java/com/example/httpconnection/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.httpconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -13,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Button send;
     private TextView status;
+
+    // ImgProcess
+    private Button wordimage;
+    private ImageView imgView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
         send = findViewById(R.id.send);
         status = findViewById(R.id.status);
+
+        // ImgProcess
+        wordimage = findViewById(R.id.wordimage);
+        imgView = findViewById(R.id.imageView);
+
+
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,8 +57,28 @@ public class MainActivity extends AppCompatActivity {
                 status.setText("데이터 수신중입니다.");
             }
         });
-    }
 
+        // ImgProcess
+        wordimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "http://noti-drawer.run.goorm.io/api/get-wordcloud";
+                String sentence = "{\"치킨\": 7,\n" +
+                        "\"피자\": 6,\n" +
+                        "\"목걸이\": -4,\n" +
+                        "\"일본\": -2}";
+                GetImageAsync getImg = new GetImageAsync(url, sentence);
+                getImg.execute();
+
+
+                send.setEnabled(false);
+                status.setText("데이터 수신중입니다.");
+
+            }
+        });
+
+    }
 
     public class GetNounInSentenceAsync extends AsyncTask<Void, Void, String> {
 
@@ -50,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         String sentence;
 
         // Constructor
-        public GetNounInSentenceAsync(String url, String sentence){
+        public GetNounInSentenceAsync(String url, String sentence) {
             this.sentence = sentence;
             this.url = url;
         }
@@ -63,17 +99,60 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String result){
+        protected void onPostExecute(String result) {
             super.onPostExecute(result);
             // 서버 통신 후 처리 완료
             // 받아온 값은 result에 저장되며 JSON 타입으로 저장됨
             // 올바르지 못한 값을 받아왔을 때, 빈 String 값이 나옴
 
             send.setEnabled(true);
-            if(result.equals(""))
+            if (result.equals(""))
                 status.setText("실패하였습니다.");
             else
                 status.setText("완료하였습니다.\n" + result);
         }
     }
+
+
+    // ImgProcess
+    public class GetImageAsync extends AsyncTask<Void, Void, Bitmap> {
+
+        String url;
+        String sentence;
+
+        // Constructor
+        public GetImageAsync(String url, String sentence) {
+            this.sentence = sentence;
+            this.url = url;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
+            // 비동기 처리 후 결과값을 리턴
+            // 이 메소드가 끝난 후에 onPostExecute()가 실행됨
+            return new GetNownInSentence().requestImg(url, sentence);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            // 서버 통신 후 처리 완료
+            // 받아온 값은 result에 저장되며 JSON 타입으로 저장됨
+            // 올바르지 못한 값을 받아왔을 때, 빈 String 값이 나옴
+
+
+
+            send.setEnabled(true);
+            if (result==null)
+                status.setText("실패하였습니다.");
+            else {
+                status.setText("완료하였습니다.");
+                imgView.setImageBitmap(result);
+            }
+
+        }
+    }
+
+
+
 }
