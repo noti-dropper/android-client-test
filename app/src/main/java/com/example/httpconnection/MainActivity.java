6c6802c7457dacf70c402551aeef544bad3c29b6
dokysp <uhug@naver.com>
2019-10-24 17:18:07 +0900

    코드 정리

--- app/src/main/java/com/example/httpconnection/MainActivity.java
+++ app/src/main/java/com/example/httpconnection/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.httpconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        // textProcess
         send = findViewById(R.id.send);
         status = findViewById(R.id.status);
 
@@ -41,16 +39,17 @@ public class MainActivity extends AppCompatActivity {
         wordimage = findViewById(R.id.wordimage);
         imgView = findViewById(R.id.imageView);
 
-
-
+        // textProcess
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String url = "http://noti-drawer.run.goorm.io/api/analyze-sentence";
-                String sentence = "{\"sentence\": \"오늘은 치킨 섭취를 먹구 싶어용.\"}";
+//                String url = "http://noti-drawer.run.goorm.io/api/analyze-sentence";
+////                String jsonData = "{\"sentence\": \"오늘은 치킨 섭취를 먹구 싶어용.\"}";
+                String url = "http://noti-drawer.run.goorm.io/api/similarity-analysis";
+                String jsonData = "{\"request_noun\" : [\"하늘\"],\"total_nouns\" : [\"오늘\",\"치킨\",\"하늘\"]}";
 
-                GetNounInSentenceAsync getNoun = new GetNounInSentenceAsync(url, sentence);
+                GetNounInSentenceAsync getNoun = new GetNounInSentenceAsync(url, jsonData);
                 getNoun.execute();
 
                 send.setEnabled(false);
@@ -64,11 +63,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String url = "http://noti-drawer.run.goorm.io/api/get-wordcloud";
-                String sentence = "{\"치킨\": 7,\n" +
+                String jsonData = "{\"치킨\": 7,\n" +
                         "\"피자\": 6,\n" +
                         "\"목걸이\": -4,\n" +
                         "\"일본\": -2}";
-                GetImageAsync getImg = new GetImageAsync(url, sentence);
+                GetImageAsync getImg = new GetImageAsync(url, jsonData);
                 getImg.execute();
 
 
@@ -83,11 +82,11 @@ public class MainActivity extends AppCompatActivity {
     public class GetNounInSentenceAsync extends AsyncTask<Void, Void, String> {
 
         String url;
-        String sentence;
+        String jsonData;
 
         // Constructor
-        public GetNounInSentenceAsync(String url, String sentence) {
-            this.sentence = sentence;
+        public GetNounInSentenceAsync(String url, String jsonData) {
+            this.jsonData = jsonData;
             this.url = url;
         }
 
@@ -95,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(Void... voids) {
             // 비동기 처리 후 결과값을 리턴
             // 이 메소드가 끝난 후에 onPostExecute()가 실행됨
-            return new GetNownInSentence().request(url, sentence);
+            return new ApiCommUtil().requestJson(url, jsonData);
         }
 
         @Override
@@ -118,11 +117,11 @@ public class MainActivity extends AppCompatActivity {
     public class GetImageAsync extends AsyncTask<Void, Void, Bitmap> {
 
         String url;
-        String sentence;
+        String jsonData;
 
         // Constructor
-        public GetImageAsync(String url, String sentence) {
-            this.sentence = sentence;
+        public GetImageAsync(String url, String jsonData) {
+            this.jsonData = jsonData;
             this.url = url;
         }
 
@@ -130,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
         protected Bitmap doInBackground(Void... voids) {
             // 비동기 처리 후 결과값을 리턴
             // 이 메소드가 끝난 후에 onPostExecute()가 실행됨
-            return new GetNownInSentence().requestImg(url, sentence);
+            return new ApiCommUtil().requestImg(url, jsonData);
         }
 
         @Override
@@ -139,8 +138,6 @@ public class MainActivity extends AppCompatActivity {
             // 서버 통신 후 처리 완료
             // 받아온 값은 result에 저장되며 JSON 타입으로 저장됨
             // 올바르지 못한 값을 받아왔을 때, 빈 String 값이 나옴
-
-
 
             send.setEnabled(true);
             if (result==null)
